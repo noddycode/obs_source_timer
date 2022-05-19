bb0b49ff67ad27a55a618dcf1da2fb528ce17a6f
Harpy <neorescuer@gmail.com>
2022-05-18 23:40:19 -0400

    Add basic functionality for configuring multiple sources

--- obs_source_timer.py
+++ obs_source_timer.py
@@ -7,8 +7,19 @@ time_multipliers = {
 }
 
 active_flag = False
+source_dict = {}
 
-class ScriptSettings():
+
+# Simple class to hold settings after each update
+class CurrentSettings():
+    source_name = ""
+    frequency = 0
+    frequency_unit = ""
+    duration = 0
+    duration_unit = ""
+
+
+class SourceController():
 
     def __init__(self, source_name, frequency, frequency_unit, duration, duration_unit):
         self.source_name = source_name
@@ -17,12 +28,78 @@ class ScriptSettings():
         self.duration = duration
         self.duration_unit = duration_unit
 
+    def set_visibility(self, is_visible):
+        current_scene = obs.obs_scene_from_source(obs.obs_frontend_get_current_scene())
+        scene_item = obs.obs_scene_find_source(current_scene, self.source_name)
+        obs.obs_sceneitem_set_visible(scene_item, is_visible)
+        obs.obs_scene_release(current_scene)
+
+    def duration_timer(self):
+        print("hi")
+        self.set_visibility(False)
+        obs.timer_add(self.frequency_timer, self.frequency * time_multipliers[self.frequency_unit])
+        # obs.timer_remove(self.duration_timer)
+        obs.remove_current_callback()
+
+    def frequency_timer(self):
+        print("hello")
+        self.set_visibility(True)
+        obs.timer_add(self.duration_timer, self.duration * time_multipliers[self.duration_unit])
+        # obs.timer_remove(self.frequency_timer)
+        obs.remove_current_callback()
+
+    def stop_timers(self, props, prop):
+        obs.timer_remove(self.duration_timer)
+        obs.timer_remove(self.frequency_timer)
+
+    def start_timers(self, props, prop):
+        self.stop_timers(None, None)
+        self.frequency_timer()
 
 def get_all_source_names():
     sources = obs.obs_enum_sources()
     source_names = [obs.obs_source_get_name(s) for s in sources]
     obs.source_list_release(sources)
     return sorted(source_names)
+
+
+def add_update_controller(props, prop):
+    global source_dict
+
+    source_dict[CurrentSettings.source_name] = SourceController(
+        CurrentSettings.source_name, 
+        CurrentSettings.frequency, 
+        CurrentSettings.frequency_unit, 
+        CurrentSettings.duration, 
+        CurrentSettings.duration_unit)
+
+
+def set_existing_properties(props, prop, settings):
+    if CurrentSettings.source_name not in source_dict:
+        return True
+
+    controller = source_dict[CurrentSettings.source_name]
+
+    obs.obs_data_set_int(settings, "frequency", controller.frequency)
+    obs.obs_data_set_string(settings, "frequency_unit", controller.frequency_unit)
+    obs.obs_data_set_int(settings, "duration", controller.duration)
+    obs.obs_data_set_string(settings, "duration_unit", controller.duration_unit)
+    obs.obs_properties_apply_settings(props, settings)
+
+    return True
+
+def start_timers(props, prop):
+    controller = source_dict[CurrentSettings.source_name]
+    controller.start_timers(props, prop)
+
+def stop_timers(props, prop):
+    controller = source_dict[CurrentSettings.source_name]
+    print(controller)
+    controller.stop_timers(props, prop)
+
+def stop_all_timers(props, prop):
+    for controller in source_dict.values():
+        controller.stop_timers(props, prop)
 
 
 def script_properties():
@@ -52,58 +129,23 @@ def script_properties():
     for unit in time_multipliers:
         obs.obs_property_list_add_string(obs_unit_list, unit + "(s)", unit)
 
-    obs.obs_properties_add_button(props, "start_button", "Start", start_timers)
-    obs.obs_properties_add_button(props, "stop_button", "Stop", stop_timers)
+    obs.obs_properties_add_button(props, "add_update_button", "Add/Update", add_update_controller)
+    obs.obs_property_set_modified_callback(obs_scene_list, set_existing_properties)
+
+    obs.obs_properties_add_button(props, "start_button", "Start Timer", start_timers)
+    obs.obs_properties_add_button(props, "stop_button", "Stop Timer", stop_timers)
+    obs.obs_properties_add_button(props, "stop_all_button", "Stop All", stop_all_timers)
 
     return props
 
-
-def stop_timers(_, _2):
-    obs.timer_remove(duration_timer)
-    obs.timer_remove(frequency_timer)
-
-
-def start_timers(_, _2):
-    stop_timers(None, None)
-    duration_timer()
-
-
-def set_visibility(is_visible):
-    current_scene = obs.obs_scene_from_source(obs.obs_frontend_get_current_scene())
-    scene_item = obs.obs_scene_find_source(current_scene, script_settings.source_name)
-    obs.obs_sceneitem_set_visible(scene_item, is_visible)
-    obs.obs_scene_release(current_scene)
-
-
-def duration_timer():
-    set_visibility(False)
-    obs.timer_add(frequency_timer, script_settings.frequency * time_multipliers[script_settings.frequency_unit])
-    obs.timer_remove(duration_timer)
-
-
-def frequency_timer():
-    set_visibility(True)
-    obs.timer_add(duration_timer, script_settings.duration * time_multipliers[script_settings.duration_unit])
-    obs.timer_remove(frequency_timer)
-   
 
 def script_update(settings):
     """
     Called when user updates a setting
     """
-    # Quartzi got cut in half by Home Depot associates
-    global script_settings
-
-    stop_timers(None, None)
-
-    settings_values = [
-        obs.obs_data_get_string(settings, "source"),
-        obs.obs_data_get_int(settings, "frequency"),
-        obs.obs_data_get_string(settings, "frequency_unit"),
-        obs.obs_data_get_int(settings, "duration"),
-        obs.obs_data_get_string(settings, "duration_unit")
-    ]
-
-    script_settings = ScriptSettings(*settings_values)
-    
+    CurrentSettings.source_name = obs.obs_data_get_string(settings, "source")
+    CurrentSettings.frequency = obs.obs_data_get_int(settings, "frequency")
+    CurrentSettings.frequency_unit = obs.obs_data_get_string(settings, "frequency_unit")
+    CurrentSettings.duration = obs.obs_data_get_int(settings, "duration")
+    CurrentSettings.duration_unit = obs.obs_data_get_string(settings, "duration_unit")
 
